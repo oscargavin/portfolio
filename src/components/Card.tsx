53325f649b1d2c3968b5b7d5c8f4d826a4b70591
oscargavin <oscarfgavin@gmail.com>
2023-02-28 12:56:58 +0000

    Made mobile responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,9 +11,11 @@ interface CardProps {
 
 function Card({ title, description, image, link }: CardProps) {
   return (
-    <div className="min-w-2/3 h-3/4">
+    <div className="flex h-full w-full items-stretch">
       <Link href={link}>
         <div className="flex transform flex-col items-stretch space-y-4 rounded-md border-2 border-gray-50 bg-black/10 py-6 px-6 pt-4 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-110">
+          <h1>{title}</h1>
+          <p className="font-extralight text-gray-200">{description}</p>
           <Image
             src={image}
             alt={title}
@@ -21,8 +23,6 @@ function Card({ title, description, image, link }: CardProps) {
             height={50}
             className="rounded-full border-2 border-gray-100 object-contain shadow-md"
           ></Image>
-          <h1>{title}</h1>
-          <p className="pb-2 font-extralight text-gray-200">{description}</p>
         </div>
       </Link>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { TwitterOutlined, LinkedinFilled } from "@ant-design/icons";
@@ -7,8 +8,15 @@ import Card from "~/components/Card";
 import { BsGithub } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import HeaderLink from "~/components/HeaderLink";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const Home: NextPage = () => {
+  const [active, setActive] = useState(false);
+
+  const onClick = () => {
+    setActive(!active);
+  };
+
   return (
     <>
       <Head>
@@ -16,31 +24,52 @@ const Home: NextPage = () => {
         <meta name="description" content="A portfolio for Oscar Gavin" />
         <link rel="icon" href="/logo.png" />
       </Head>
-      <main className="linear-gradient h-full pb-16 font-serif text-gray-50">
-        <div className="mx-auto flex w-full justify-around border-b-2 border-gray-50 pt-8">
+      <main className="linear-gradient h-full w-full pb-16 font-serif text-gray-50">
+        <div className="flex w-full items-center justify-between border-b-2 border-gray-50 pt-8">
           <Link
             href="/"
-            className="flex flex-row items-center space-x-4 pb-6 transition "
+            className="ml-4 flex flex-row items-center space-x-4 pb-6 transition "
           >
             <Image
               src="/logo.png"
               alt="OGavin"
               width={75}
               height={75}
-              className="w-1/4 rounded-full md:w-1/2"
+              className="w-1/3 rounded-full md:w-1/2"
             />
-            <h1 className="text-lg font-extralight md:text-2xl">OGavin</h1>
+            <h1 className="text-xl font-extralight md:text-2xl">OGavin</h1>
           </Link>
-          <div className="flex flex-row items-center space-x-4 pb-6">
-            <HeaderLink title="Home" href="/" />
-            <HeaderLink title="Skills" href="/" />
-            <HeaderLink title="Projects" href="/" />
-            <HeaderLink title="Blog" href="/blog" />
-            <HeaderLink title="Contact" href="/contact" />
+          <div className="relative">
+            <HiOutlineMenu
+              className={`${
+                active ? "rotate-90" : "rotate-0"
+              } absolute right-6 bottom-1 z-20 transition ease-in-out lg:hidden`}
+              width={50}
+              height={50}
+              onClick={onClick}
+            ></HiOutlineMenu>
+            <div className="absolute right-6 -top-3 hidden flex-row items-center space-x-4 pb-6 lg:flex">
+              <HeaderLink title="Home" href="/" />
+              <HeaderLink title="Skills" href="/" />
+              <HeaderLink title="Projects" href="/" />
+              <HeaderLink title="Blog" href="/blog" />
+              <HeaderLink title="Contact" href="/contact" />
+            </div>
           </div>
         </div>
-        <div className="mx-auto flex h-2/5 w-full items-center justify-around border-b-2 border-gray-50 bg-black/10 px-6 drop-shadow-lg backdrop-blur-lg">
-          <div className="flex w-1/3 flex-col">
+        <div
+          className={`${
+            active ? "flex" : "hidden"
+          } flex-col space-y-2 border-b-2 border-gray-50 bg-black/10 py-6 px-6 drop-shadow-lg backdrop-blur-lg`}
+        >
+          <HeaderLink title="Home" href="/" />
+          <HeaderLink title="Skills" href="/" />
+          <HeaderLink title="Projects" href="/" />
+          <HeaderLink title="Blog" href="/blog" />
+          <HeaderLink title="Contact" href="/contact" />
+        </div>
+        <div className="flex w-full items-center justify-between border-b-2 border-gray-50 bg-black/10 py-6 px-6 drop-shadow-lg backdrop-blur-lg">
+          <div className="flex flex-col">
             <div className="mb-6 flex flex-row space-x-4">
               <Link
                 href="https://twitter.com/Oscar_Gav"
@@ -61,10 +90,10 @@ const Home: NextPage = () => {
                 <BsGithub />
               </Link>
             </div>
-            <h1 className="pb-2 text-lg font-semibold text-gray-50 md:text-2xl">
+            <h1 className="text-md pb-2 font-semibold text-gray-50 md:text-2xl lg:text-lg">
               I am Oscar Gavin...
             </h1>
-            <p className="text-md font-extralight text-gray-100 md:text-lg">
+            <p className="lg:text-md text-sm font-extralight text-gray-100 md:text-lg">
               an Artificial Intelligence and Computer Science graduate, with
               knowledge in full-stack development and machine learning
             </p>
@@ -75,6 +104,7 @@ const Home: NextPage = () => {
               alt="OGavin"
               width={300}
               height={300}
+              className="w-2/3 lg:w-full"
             ></Image>
           </div>
         </div>
@@ -85,7 +115,7 @@ const Home: NextPage = () => {
               My Expertise
             </h1>
           </div>
-          <div className="mx-8 flex min-h-fit flex-grow flex-row space-x-8">
+          <div className="mx-3 flex flex-col space-y-4 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-4">
             <Card
               title="Web Development"
               description="I have experience in full-stack development, with a focus on the front-end. I have worked with React, Next.js, Prisma, tRPC, and Tailwind CSS."
@@ -129,12 +159,12 @@ const Home: NextPage = () => {
             <p>oscarfgavin@gmail.com</p>
           </div>
         </div>
-        <div className="mt-8">
+        <div className="mx-4 mt-8 lg:mx-16 ">
           <div className="mb-8 flex flex-col items-center justify-center">
             <p className="mb-2 text-xs font-bold text-gray-200">PROJECTS</p>
             <h1 className="text-2xl font-semibold text-gray-50">My Work</h1>
           </div>
-          <div className="mx-16 mb-12 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
+          <div className="mb-10 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
             <div className="mr-4">
               <h1 className="pb-2 font-semibold">Landmark Localisation</h1>
               <p className="font-extralight text-gray-200">
@@ -157,7 +187,7 @@ const Home: NextPage = () => {
               className="rounded-sm object-contain shadow-md"
             ></Image>
           </div>
-          <div className="mx-16 mb-12 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
+          <div className="mb-10 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
             <div className="mr-4">
               <h1 className="pb-2 font-semibold">Cardology.io</h1>
               <p className="font-extralight text-gray-200">
@@ -179,7 +209,7 @@ const Home: NextPage = () => {
               className="rounded-sm object-cover shadow-md"
             ></Image>
           </div>
-          <div className="mx-16 mb-12 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
+          <div className="mb-10 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
             <div className="mr-4">
               <h1 className="pb-2 font-semibold">
                 Chess Piece and Board Position Classifier
@@ -203,7 +233,7 @@ const Home: NextPage = () => {
               className="rounded-sm object-contain"
             ></Image>
           </div>
-          <div className="mx-16 mb-12 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
+          <div className="mb-10 flex min-h-fit transform flex-col items-center space-y-4 rounded-md border-2 border-gray-50 bg-black/10 p-6 drop-shadow-lg backdrop-blur-lg transition duration-500 ease-in-out hover:scale-105 md:flex-row">
             <div className="mr-4">
               <h1 className="pb-2 font-semibold">
                 Sentiment Analyser for Rotten Tomatoes Reviews
